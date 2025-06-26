a58f52e92dd5ccd809213269188c41ff2c1cd157
Dominik Bartsch <dbartsch@nd.edu>
2025-06-26 15:15:06 -0500

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,10 @@
+#vehicle.py
+#Author: Alex Kult
+#Description: Data on the physical properties of the launch vehicle
+#Date: 6-9-2025
+#Copyright Alpha Kappa Sigma
+
+# --- Parameters ---
+cp_cg = 0.42 #Distance between center of gravity and center of pressure [m]
+dry_mass = 17.625 #Mass of rocket after motor burn [kg]
+mom_inertia = 8.04 #Moment of inertial of rocket after motor burn [kg*m^2]
